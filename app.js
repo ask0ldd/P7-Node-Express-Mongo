f38f9af8cf47a2b754ae2b903e8ca9037b979c4f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-01 05:05:09 +0100

    bdd integration fu

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 const express = require('express')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+
 
 const app = express()
+const Book = require('./models/Book')
 
-app.use((req, response, next)=> {
+/*app.use((req, response, next)=> {
     console.log("requete recue") 
     next()
-}) // console > powershell et non console browser
+}) // console > powershell et non console browser*/
 
-app.use((req, response, next)=> {
-    response.status(201)
-    next()
-})
+app.use(express.json()); // permet d'extraire le corps json de la requete
 
-app.use((req, response, next)=> {
-    response.json({message : "votrerequetebienrecue"})
-}) // middleware
+app.use((req, res, next) => { // header to deal with CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
+app.use(bodyparser.json())
 
-
-
+app.use('/api/stuff', (req, res, next) => { // middleware
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app // permet d'acceder a l'application depuis nos autres fichiers, notamment serveur node
--- server.js
+++ server.js
@@ -1,8 +1,49 @@
 const http = require('http')
+const app = require('./app')
 
-const server = http.createServer((req, resp)=>{
-    resp.end("reponseduserveurezaeazeaz")
-}) // fonction qui sera appelee pour chaque requete recue par le serveur
+const normalizePort = val => { // if val : string > convert to a number; if the result isn't a number > false
+  const port = parseInt(val, 10);
 
-server.listen(process.env.PORT || 3000) // au cas ou port 3000 indispo, ou simplement si l'environnement ou tourne serveur nous communique un port a utiliser
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+
+const port = normalizePort(process.env.PORT || '3000'); // if port 3000 unav or if the env running the server send us another port to use
+app.set('port', port);
+
+const server = http.createServer(app); // function that will be called each time the server receive a request, here the express app is passed
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
 
