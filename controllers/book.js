af08e878f7d03f4602831c2edf3cbe83373fa443
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-12 15:05:15 +0100

    got rid of most useless comments

--- controllers/book.js
+++ controllers/book.js
@@ -1,16 +1,12 @@
 const Book = require('../models/Book')
 
 /*
-
 si passage multer : 
 req.body.file + req.body.book
 autrement
 req.body
-
 */
 
-
-// verifier que le book n'existe pas deja ?
 exports.postBook = (req, res, next) => {
     const parsedBook = JSON.parse(req.body.book);
     delete parsedBook.userId // id could be falsified, so we get it through auth
@@ -24,43 +20,37 @@ exports.getBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
     .then(book => 
         {
-            book.averageRating = book.averageRating.toFixed(1) // pour ne renvoyer qu'une decimale
+            book.averageRating = book.averageRating.toFixed(1) // limited number of decimals
             res.status(200).json(book)
         })
     .catch(error => res.status(404).json({ error }))
 }
 
-exports.updateBook = (req, res, next) => { //verifie si bon user?
+exports.updateBook = (req, res, next) => {
     const tempBook = req.file ? {...JSON.parse(req.body.book), imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : {...req.body}
     delete tempBook.userId // not replaced cause no need to update that field
 
     Book.findOne({ _id: req.params.id })
     .then(book => 
         {
-            if(book.userId === req.auth.userId)
+            if(book.userId === req.auth.userId) // before update check if authorized user
             {
-                Book.updateOne({ _id: req.params.id}, {...tempBook}).then(res.status(200).json("updated")).catch()
+                Book.updateOne({ _id: req.params.id}, {...tempBook})
+                .then(res.status(201).json("Book updated"))
+                .catch(error => res.status(400).json({ error }))
             }else
             {
                 res.status(401).json({ message : 'Not authorized'});
             }
         })
     .catch(error => res.status(400).json({ error }))
-    /*Book.updateOne(
-        { _id: req.bookId}, // select // res -> req
-        { $set :{ "averageRating" : req.bookAvg }} // $ = first result // res -> req
-        )
-        .then(books => console.log("updated avg rating"))
-        .catch(error => console.log({ error }))*/
-
-        //utiliser save
 }
 
 exports.deleteBook = (req, res, next) => { // verify user?
     console.log(req.params.id)
     Book.deleteOne({ _id: req.params.id })
-    .then(book => res.status(200).json("deleted"))
-    .catch(error => res.status(404).json({ error }))
+    .then(book => res.status(200).json("Book deleted"))
+    .catch(error => res.status(400).json({ error }))
 }
 
 exports.getAllBooks = (req, res, next) => { 
@@ -75,8 +65,7 @@ exports.getTop = (req, res, next) => {
     .catch(error => res.status(404).json({ error }))
 }
 
-exports.postRating = (req, res, next) => { // verifier que l'utilisateur n'a pas deja poste une note via middleware?
-    //const {userId, rating} = req.body // recuperer userid via token?
+exports.postRating = (req, res, next) => {
     console.log(req.body)
     const {userId, rating} = req.body
     console.log(req.params.id)
@@ -84,17 +73,16 @@ exports.postRating = (req, res, next) => { // verifier que l'utilisateur n'a pas
         { _id: req.params.id },
         { $push: {ratings : { userId : req.auth.userId, grade : rating }} }) // id recupere via l'auth par sécurité
         .then(books => {
-            res.status(201).json("rating posted.") // renvoyer le book
-            req.bookId = req.params.id // value to pass to the next middleware
+            res.status(200).json("Rating posted.") // renvoyer le book N!
+            req.bookId = req.params.id
             next()})
-        .catch(error => res.status(404).json({ error }))
+        .catch(error => res.status(400).json({ error }))
 }
 
 exports.updateAvgRating = (req, res, next) => {
-    //console.log(req.locals.id) // res -> req
     Book.updateOne(
-        { _id: req.bookId}, // select // res -> req
-        { $set :{ "averageRating" : req.bookAvg }} // $ = first result // res -> req
+        { _id: req.bookId},
+        { $set :{ "averageRating" : req.bookAvg }} // $ = first result
         )
         .then(books => console.log("updated avg rating"))
         .catch(error => console.log({ error }))
@@ -109,22 +97,5 @@ exports.updateAvgRating = (req, res, next) => {
         { _id: bookid },
         { author : "joe abercrombie"})
         .then(books => res.status(201).json("okdoki"))
-        .catch(error => res.status(404).json({ error }))
-}*/
-
-// bestrating 3 books avec la meilleure note
-
-// 4.5 => 45
-
-//utilisateur ne doit pas pouvoir noter un livre plusieurs fois
-
-
-/*exports.updateRating = (req, res, next) => {
-    const {bookid, userid, grade}= req.body // recuperer userid via token?
-    Book.updateOne(
-        { _id: bookid, 'ratings.userId' : userid}, // select
-        { $set :{ "ratings.$.grade" : grade }} // $ = first result
-        )
-        .then(books => res.status(201).json("updated rating"))
         .catch(error => res.status(404).json({ error }))
 }*/
--- middleware/avgrating.js
+++ middleware/avgrating.js
@@ -1,29 +1,7 @@
 const Book = require('../models/Book')
 const mongoose = require('mongoose');
 
-/*module.exports = (req, res, next) => {
-    try {
-        //next()
-
-        //console.log(Book.findOne({ _id: "63b4a70e516c692da3c9d477" }).aggregate(([{$group: {avgrating:{$avg:"$points"}}}])))
-        Book.aggregate([
-            { $match : {_id : mongoose.Types.ObjectId(res.locals.id)} }, // select
-            { $unwind : "$ratings" }, // create one full document per element in the array 
-            { "$group" : {
-                "_id" : null, // mandatory
-                "avg" : { "$avg" : "$ratings.grade" }}}])
-            .then(result => {
-                console.log(result[0].avg)
-                res.locals.avg = result[0].avg
-                next()})
-            .catch(error => console.log(error))     
-    } catch(error) {
-        //res.status(401).json({ error })
-        console.log(error)
-    }
- }*/
-
- module.exports = (req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         Book.aggregate([{ $match: { _id: mongoose.Types.ObjectId(req.bookId) } }])
         .unwind("$ratings")
