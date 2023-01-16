7d2eabefbd65819403619f02a0c17fe0d4b9a131
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-16 18:39:17 +0100

    trimed values model

--- models/Book.js
+++ models/Book.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const bookSchema = mongoose.Schema({
     userId: { type : String, required : [true, 'UserId is missing'], validate: [/^[a-z0-9]{5,40}$/, 'Invalid user id']},
-    title: { type : String, required : [true, 'Title is missing'], minLength: [2, 'Title is too short'], maxLength: [24, 'Title is too long']},
-    author: { type : String, required : [true, 'Author is missing'], minLength: [4, 'Name is too short'], maxLength: [24, 'Name is too long']},
+    title: { type : String, trim : true, required : [true, 'Title is missing'], minLength: [2, 'Title is too short'], maxLength: [24, 'Title is too long']},
+    author: { type : String, trim : true, required : [true, 'Author is missing'], minLength: [4, 'Name is too short'], maxLength: [24, 'Name is too long']},
     imageUrl: { type : String, required : true},
-    year: { type : Number, required : [true, 'Year of publication is missing'], validate: [/^[0-9]{3,4}$/, 'Invalid Year']},
-    genre: { type : String, required : [true, 'Genre is missing'], minLength: [4, 'Genre is too short'], maxLength: [24, 'Genre is too long']},
+    year: { type : Number, trim : true, required : [true, 'Year of publication is missing'], validate: [/^[0-9]{3,4}$/, 'Invalid Year']},
+    genre: { type : String, trim : true, required : [true, 'Genre is missing'], minLength: [4, 'Genre is too short'], maxLength: [24, 'Genre is too long']},
     ratings:    [{
                     _id : false,
                     userId: {type : String, required : true},
--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type : String, required : true, unique : [true, 'Email already used'], maxLength: [64, 'Email is too long'], validate: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format.']}, // unique
-    password: { type : String, required : true, minLength: 8, maxLength: 72} // longueur minimum (?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ] / hash length 72 max
+    email: { type : String, trim : true, required : true, unique : [true, 'Email already used'], maxLength: [64, 'Email is too long'], validate: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format.']}, // unique
+    password: { type : String, required : true, minLength: 8, maxLength: 72} // hash length 72 chars max
 })
 
 userSchema.plugin(uniqueValidator);
